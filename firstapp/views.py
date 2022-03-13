5e22f1682b2e9d8fba203ac80cae05271ee60c8f
go9amn <semenkaurin@gmail.com>
2022-03-13 21:15:01 +0300

    Первая часть

---
+++ firstapp/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    name = request.GET.get('name', '')
+    message = request.GET.get('message', 'Давай дружить!')
+    output = '<h2>Hello {0}<h3>{1}</h3></h2>'.format(name, message)
+    return HttpResponse(output)
